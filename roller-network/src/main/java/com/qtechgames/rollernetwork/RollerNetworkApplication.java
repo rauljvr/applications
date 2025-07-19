38ead09c49e2f8a0bc6b35f21085930c0c45ba81
Raul Verastegui <raulv.one@gmail.com>
2025-07-19 10:16:11 +0200

    initial project structure

---
+++ roller-network/src/main/java/com/qtechgames/rollernetwork/RollerNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.qtechgames.rollernetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollerNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollerNetworkApplication.class, args);
+	}
+
+}
