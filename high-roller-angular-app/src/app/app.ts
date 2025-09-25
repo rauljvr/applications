e247fd5faca39a0d81229a9e8eaa8d529750f08d
Raul Verastegui <raulv.one@gmail.com>
2025-09-25 13:02:19 +0200

    remove unused component

--- high-roller-angular-app/src/app/app.ts
+++ high-roller-angular-app/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Highroller } from './components/highroller/highroller';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Highroller],
+  imports: [Highroller],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
